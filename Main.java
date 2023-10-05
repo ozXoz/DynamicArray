1d98df50563571be0e692bbfdc0985e630a093c3
ozXoz <honourkorkmaz@gmail.com>
2023-10-05 16:19:37 -0400

    dynamic array

---
+++ Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dynamicarray;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author oz
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ArrayList<String> names = new ArrayList<>();
+        
+        names.add("test");
+          names.add("test2");
+        System.out.println(names);
+    }
+    
+}
